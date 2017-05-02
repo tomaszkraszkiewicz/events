edbbfa1bff14979601f121fae135e799f0bd84e5
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-05-02 15:57:30 +0200

    changed function

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,5 @@ var	add = document.getElementById('addElem');
 var tag = document.getElementsByTagName('li').lenght;
 
 add.addEventListener('click', function () {
-	for (i = 0; i > tag; i++)
 		list.innerHTML += '<li>item</li>';
 }) 
